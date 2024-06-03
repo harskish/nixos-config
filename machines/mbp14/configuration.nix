968bb2aa84ff27845f5571e4dd85989da59235e4
Erik Härkönen <erik.harkonen@hotmail.com>
2024-06-03 11:31:51 +0300

    MBP14 tweaking

--- machines/mbp14/configuration.nix
+++ machines/mbp14/configuration.nix
@@ -11,49 +11,57 @@
       ./apple-silicon-support
     ];
 
+  # Specify path to peripheral firmware files
+  # => seems broken
+  # hardware.asahi.peripheralFirmwareDirectory = ./firmware;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
   
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
+  # Networking
+  networking.hostName = "nixos"; # hostname.
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
   networking.wireless.iwd = {
     enable = true;
     settings.General.EnableNetworkConfiguration = true;
   };
 
-  # Specify path to peripheral firmware files
-  # hardware.asahi.peripheralFirmwareDirectory = ./firmware;
-
   # Set your time zone.
   time.timeZone = "Europe/Helsinki";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    #font = "Lat2-Terminus16";
-    keyMap = "fi";
-    #useXkbConfig = true; # inherit from config.services.xserver
+
+  # Aliases
+  environment.shellAliases = {
+    # NB: using ~ might create dummy ./~ dirs everywhere
+    nixconf = "sudo code --no-sandbox --user-data-dir=$HOME/.vscode /etc/nixos/configuration.nix";
+    gst = "git status";
+    slog = "git log --oneline -20";
+  };
+  
+  # Configure keymap
+  console.keyMap = "fi";
+  services.xserver.xkb.layout = "fi"; # needed despite no x11
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Pure wayland Plasma 6
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.enable = false;
+  services.displayManager = {
+    defaultSession = "plasma";
   };
 
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-
-  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  # Enable OpenGL
+  hardware.opengl.enable = true;
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -73,15 +81,18 @@
   users.users.erik = {
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
   };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
+    vscode
+    brave
+    unixtools.ping
   ];
 
   programs.git.enable = true;
@@ -91,46 +102,6 @@
     init.defaultBranch = "main";
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
 
